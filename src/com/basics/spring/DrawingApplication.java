39b70afbcd6f8321e9836134c6257a5c180c2cc8
kashish <kashish.bhatia@nl.abnamro.com>
2017-06-17 19:09:09 +0200

    feat: Complete tutorial for Constructor/Property injection.

---
+++ src/com/basics/spring/DrawingApplication.java
@@ -0,0 +1,16 @@
+package com.basics.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class DrawingApplication {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new FileSystemXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle)context.getBean("triangle");
+		
+		triangle.draw();
+	}
+
+}
