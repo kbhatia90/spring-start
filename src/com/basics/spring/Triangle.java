7d295233496c122bef0acb20e9c9e12c9dad57df
kashish <kashish.bhatia@nl.abnamro.com>
2017-06-17 19:30:03 +0200

    feat: Do bean injection in the xml

--- src/com/basics/spring/Triangle.java
+++ src/com/basics/spring/Triangle.java
@@ -1,34 +1,40 @@
 package com.basics.spring;
 public class Triangle {
 
-	private String type;
-	private int height;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 	
-	/*public int getHeight() {
-		return height;
-	}*/
-
-	public Triangle(String type){
-		this.type = type;
-	}
-	
-	public Triangle(String type, int height){
-		this.height= height;
-		this.type=type;
-	}
-	
-	
-	/*public String getType() {
-		return type;
+		public Point getPointA() {
+		return pointA;
 	}
 
-	public void setType(String type) {
-		this.type = type;
-	}*/
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
 
 	public void draw()
 	{
-		System.out.println("Drawn " + type + " triangle with height " + height);
+		System.out.println(pointA.getX()+" "+pointA.getY());
+		System.out.println(pointB.getX()+" "+pointB.getY());
+		System.out.println(pointC.getX()+" "+pointC.getY());
 	}
 	
 }
