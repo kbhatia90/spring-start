1151b9c29b27290d32bd026d904690a86f5311c6
kashish <kashish.bhatia@nl.abnamro.com>
2017-06-17 20:13:03 +0200

    feat: auto wire the beanss

--- src/com/basics/spring/Triangle.java
+++ src/com/basics/spring/Triangle.java
@@ -3,21 +3,43 @@ package com.basics.spring;
 import java.util.List;
 
 public class Triangle {
-	
-	private List<Point> points;
 
-	public List<Point> getPoints() {
-		return points;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public void setPoints(List<Point> points) {
-		this.points = points;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
 
 	public void draw()
 	{
-		for (Point point : points)
-				System.out.println(point.getX()+" "+point.getY());
-	}
+
+		System.out.println(pointA.getX()+" "+pointA.getY());
+		System.out.println(pointB.getX()+" "+pointB.getY());
+		System.out.println(pointC.getX()+" "+pointC.getY());
 	
+	}
+
 }
